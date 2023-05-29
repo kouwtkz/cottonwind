8b115d4b1dc153afe4d5b62ae86c5660fe770add
kouwtkz <kr.smn6vbg@gmail.com>
2023-05-30 08:51:38 +0900

    ライトボックスの調整

--- mysite/static/common/js/gallery.js
+++ mysite/static/common/js/gallery.js
@@ -1,18 +1,23 @@
 window.addEventListener("DOMContentLoaded", () => {
-    var lightbox = document.querySelector(".lightbox");
-    var preview_img = lightbox.querySelector(".preview img.viewer");
-    var loading_img = lightbox.querySelector(".preview img.loading");
-    var preview_imgLink = lightbox.querySelector(".preview a.link");
-    var preview_text = lightbox.querySelector(".preview_text");
-    var preview_title = preview_text.querySelector(".title");
-    var preview_description = preview_text.querySelector(".description");
-    var preview_date = preview_text.querySelector(".date");
-    var preview_link = preview_text.querySelector(".link a");
-    var lightboxSync = (anim = true) => {
+    const lightbox = document.querySelector(".lightbox");
+    const lightboxWindow = lightbox.querySelector(".window");
+    const preview_img = lightbox.querySelector(".preview img.viewer");
+    const loading_img = lightbox.querySelector(".preview img.loading");
+    const preview_imgLink = lightbox.querySelector(".preview a.link");
+    const preview_text = lightbox.querySelector(".preview_text");
+    const preview_title = preview_text.querySelector(".title");
+    const preview_description = preview_text.querySelector(".description");
+    const preview_date = preview_text.querySelector(".date");
+    const preview_link = preview_text.querySelector(".link a");
+    const lightboxSync = (anim = true) => {
         if (location.hash !== "") {
             document.body.classList.add("showLightbox");
-            var targetImg = document.querySelector(`a[href="${location.hash}"] img`);
-            var img_src = decodeURI(decodeURIComponent(location.hash)).slice(1);
+            const targetImg = document.querySelector(
+                `a[href="${location.hash}"] img`
+            );
+            const img_src = decodeURI(decodeURIComponent(location.hash)).slice(
+                1
+            );
             preview_img.src = targetImg.src;
             loading_img.src = img_src;
             preview_img.alt = targetImg.alt;
@@ -27,16 +32,17 @@ window.addEventListener("DOMContentLoaded", () => {
             if (typeof twemoji !== "undefined") {
                 twemoji.parse(preview_text);
             }
+            lightboxWindow.scrollTo(0, 0);
             if (anim) {
                 gsap.fromTo(
-                    ".lightbox .window",
+                    lightboxWindow,
                     { opacity: 0, scale: 0.95 },
                     { opacity: 1, delay: 0.05, duration: 0.25, scale: 1 }
                 );
                 gsap.fromTo(
-                    ".lightbox",
-                    { opacity: 0 },
-                    { opacity: 1, delay: 0.02, duration: 0.28 }
+                    lightbox,
+                    { opacity: 0, scrollTo: 0 },
+                    { opacity: 1, delay: 0.02, duration: 0.28, scrollTo: 0 }
                 );
             }
         } else {
@@ -45,16 +51,17 @@ window.addEventListener("DOMContentLoaded", () => {
                     gsap.fromTo(
                         ".lightbox .window",
                         { opacity: 1, scale: 1 },
-                        { opacity: 0, scale: 0.95, duration: 0.20 },
+                        { opacity: 0, scale: 0.95, duration: 0.2 }
                     );
                     gsap.fromTo(
                         ".lightbox",
                         { opacity: 1 },
                         {
-                            opacity: 0, duration: 0.20,
+                            opacity: 0,
+                            duration: 0.2,
                             onComplete: () => {
                                 document.body.classList.remove("showLightbox");
-                            }
+                            },
                         }
                     );
                 } else {
@@ -63,7 +70,7 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         }
     };
-    var lightboxBackAction = () => {
+    const lightboxBackAction = () => {
         if (history.state === null) {
             history.back();
         } else {
