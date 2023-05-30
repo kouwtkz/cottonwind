c459c0f90866a5310812e47063d41d6c4a3bcdac
kouwtkz <kr.smn6vbg@gmail.com>
2023-05-31 07:56:07 +0900

    キャッシュがバグってるので再度コミット

---
+++ mysite/_config.ts
@@ -0,0 +1,11 @@
+import lume from "lume/mod.ts";
+import pug from "lume/plugins/pug.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+
+const site = lume();
+
+site.use(pug());
+site.copy("static", ".");
+site.use(sitemap({query: "sitemap"}));
+
+export default site;
