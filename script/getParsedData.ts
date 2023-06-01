f262f8372e97efc607abf74ddc73acd0a7912904
kouwtkz <kr.smn6vbg@gmail.com>
2023-06-01 18:47:08 +0900

    隠しファイルはファイルもフォルダも共通するようにした

--- script/getParsedData.ts
+++ script/getParsedData.ts
@@ -15,39 +15,39 @@ async function getParsedData(path: string) {
     Array.from(Deno.readDirSync(path)).forEach((item) => {
         const filePath = `${path}/${item.name}`;
         let baseName = '';
-        if (item.isDirectory) {
-            if (!/^[._]/.test(item.name)) {
+        if (!/^[._]/.test(item.name)) {
+            if (item.isDirectory) {
                 baseName = item.name;
                 runList.push(getParsedData(filePath).then((data) => {
                     dicUpdate(baseName, data);
                 }));
-            }
-        } else {
-            const m = item.name.match(/^(.+)\.([^.]+)$/);
-            if (m) {
-                baseName = m[1];
-                const ext = m[2];
-                switch (ext) {
-                    case "json":
-                        runList.push(Deno.readTextFile(filePath).then((str) => {
-                            dicUpdate(baseName, JSON.parse(str));
-                        }));
-                        break;
-                    case "yaml":
-                        runList.push(Deno.readTextFile(filePath).then((str) => {
-                            dicUpdate(baseName, parse(str));
-                        }));
-                        break;
-                    case "ts":
-                    case "js":
-                        runList.push(import(`file:///${Deno.cwd()}/${filePath}`).then((data) => {
-                            dicUpdate(baseName, { ...data });
-                        }));
-                        break;
+            } else {
+                const m = item.name.match(/^(.+)\.([^.]+)$/);
+                if (m) {
+                    baseName = m[1];
+                    const ext = m[2];
+                    switch (ext) {
+                        case "json":
+                            runList.push(Deno.readTextFile(filePath).then((str) => {
+                                dicUpdate(baseName, JSON.parse(str));
+                            }));
+                            break;
+                        case "yaml":
+                            runList.push(Deno.readTextFile(filePath).then((str) => {
+                                dicUpdate(baseName, parse(str));
+                            }));
+                            break;
+                        case "ts":
+                        case "js":
+                            runList.push(import(`file:///${Deno.cwd()}/${filePath}`).then((data) => {
+                                dicUpdate(baseName, { ...data });
+                            }));
+                            break;
+                    }
                 }
             }
         }
-    })
+    });
     await Promise.all(runList);
     return dic;
 }
