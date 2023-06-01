e9f0af098c38fce0f3832ac1019b7a2c07c6b2ce
kouwtkz <kr.smn6vbg@gmail.com>
2023-06-02 07:38:31 +0900

    ちょっとすっきりする書き方にした

--- mysite/_data.ts
+++ mysite/_data.ts
@@ -2,4 +2,4 @@ import getParsedData from "../script/getParsedData.ts";
 import date_format from "../script/date_format.ts"
 const scripts = await getParsedData("_scripts");
 
-export default { ...scripts , ...{ date_format }};
+export default { ...scripts , date_format };
