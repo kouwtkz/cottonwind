039bdcba295d10eba76b3980d658349640cdc536
kouwtkz <kr.smn6vbg@gmail.com>
2023-06-07 09:03:10 +0900

    コピーしたstaticのファイルが更新されてない場合は上書きしないようにした

--- mysite/_scripts/staticCopy.ts
+++ mysite/_scripts/staticCopy.ts
@@ -1,16 +1,20 @@
 export const staticPath = "_static";
 const sitePath = "_site";
-await Deno.mkdir(sitePath).catch(() => {});
+await Deno.mkdir(sitePath).catch(() => { });
 function copyStatic(cur: string) {
   // deno-lint-ignore no-explicit-any
   Array.from(Deno.readDirSync(cur)).forEach(async (item: any) => {
     const itemPath = `${cur}/${item.name}`;
     const outputPath = itemPath.replace(staticPath, sitePath);
     if (item.isFile) {
-      await Deno.copyFile(itemPath, outputPath);
+      const itemCheck = await Deno.stat(itemPath).catch(() => null);
+      const outputCheck = await Deno.stat(outputPath).catch(() => null);
+      if (!outputCheck || (itemCheck && (<Date>itemCheck.mtime).getTime() !== (<Date>outputCheck.mtime).getTime())) {
+        await Deno.copyFile(itemPath, outputPath);
+      }
     } else if (item.isDirectory) {
       if (!/^[._]/.test(item.name)) {
-        await Deno.mkdir(outputPath).catch(() => {});
+        await Deno.mkdir(outputPath).catch(() => { });
         copyStatic(itemPath);
       }
     }
